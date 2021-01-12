b4aedd4e9847a8cd79e3893f83c2f3fdf66fc7df
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-12 19:36:38 +0000

    finalized template

--- downloader/models.py
+++ downloader/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=250)
 
-
-class Post(models.Model):
-    category=models.ManyToManyField(Category)
-    title = models.CharField(max_length=250)
-    content=models.TextField()
-    
