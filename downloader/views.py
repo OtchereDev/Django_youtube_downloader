b8dc1dfead7cfdf06846365befe8b936b1faf969
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-13 07:15:15 +0000

    add zipping functionality

--- downloader/views.py
+++ downloader/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 import pytube
-import re
+import os
+import shutil
+from os import path
 
-from pytube import YouTube
 
-playlist=pytube.Playlist('https://www.youtube.com/watch?v=sakQbeRjgwg&list=PL4cUxeGkcC9jdm7QX143aMLAqyM-jTZ2x&index=1')
-playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
-print(playlist)
-for url in playlist.video_urls:
-    youtube = pytube.YouTube(url)
-    print(youtube.streams.first())
+# from pytube import YouTube
+
+# playlist=pytube.Playlist('https://www.youtube.com/watch?v=sakQbeRjgwg&list=PL4cUxeGkcC9jdm7QX143aMLAqyM-jTZ2x&index=1')
+# playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+# print(playlist)
+# for url in playlist.video_urls:
+#     youtube = pytube.YouTube(url)
+#     print(youtube.streams.first())
     
 
 # playlist=pytube.Playlist('https://www.youtube.com/playlist?list=PL4cUxeGkcC9hKBg2mU8Hat4-NedlubC3C')
@@ -25,3 +28,15 @@ def home(request):
     video = youtube.streams.first()
     video.download('media')
     return render(request,'downloader/index.html')
+
+
+def main():
+
+    if path.exists('media/screen'):
+        src= path.realpath('media/screen')
+        print(src)
+
+    root_dir,tail=path.split(src)
+    shutil.make_archive('media/screen_zip','zip',src)
+
+main()
