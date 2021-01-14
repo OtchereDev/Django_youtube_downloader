277d4240eb5196d2c37157c3df020cba7c7abf30
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-14 04:46:54 +0000

    add media location uri

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -124,6 +124,6 @@ MEDIA_URL='/media/'
 
 STATIC_ROOT = '/static_root/'
 
-MEDIA_ROOT = '/media_root/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS=[ os.path.join(BASE_DIR,'static'),]
